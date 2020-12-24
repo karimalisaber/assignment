8d19ca420818b8ef9be62847c6fd7744a6699213
Karim Saber <ksaber@tafeel.com>
2020-12-24 14:51:05 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'code-chalenge';
+  dynamicForm: FormGroup = new FormGroup({});
+  constructor(private fb: FormBuilder){}
+
+  ngOnInit(){
+
+  }
+
+  
+  initForm(){
+    this.dynamicForm = this.fb.group({
+      name: 'karim',
+      email: 'ali'
+    })
+  }
+
 }
