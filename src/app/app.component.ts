ad50feaee860e428ca7e678d7c05caf59e67bd1d
Karim Saber <karimali201094@gmail.com>
2020-12-26 03:52:23 +0200

    end the form builder part

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,121 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, FormArray, AbstractControl } from '@angular/forms';
+
+enum ActionTypes  {
+  Label,
+  TextBox,
+  Dropdown,
+  Checkbox,
+  RadioButton
+}
+
+const DefaulUser = [
+  {
+    controler: 'name',
+    placeholder: 'name',
+    value: 'karim',
+    type: ActionTypes.Label
+  },
+  {
+    controler: 'email',
+    placeholder: 'name',
+    value: 'karim',
+    type: ActionTypes.TextBox
+  },
+
+  {
+    controler: 'test',
+    placeholder: 'name',
+    options: [
+      {id: 1, name: 'karim'},
+      {id: 2, name: 'karim'},
+      {id: 3, name: 'karim'},
+      {id: 4, name: 'karim'}
+    ],
+    value: 4,
+    type: ActionTypes.Dropdown
+  },
+  {
+    controler: 'check',
+    placeholder: 'name',
+    options: [
+      {id: 1, placeholder: 'karim', value: true , controler: 'test'},
+      {id: 2, placeholder: 'karim', value: true, controler: 'tfest'},
+      {id: 3, placeholder: 'karim', value: true, controler: 'tefst'},
+      {id: 4, placeholder: 'karim', value: true, controler: 'tesft'}
+    ],
+    value: null,
+    type: ActionTypes.Checkbox
+  },
+  {
+    controler: 'namfe',
+    placeholder: 'name',
+    value: 3,
+    options: [
+      {id: 1, placeholder: 'karim', controler: 'test'},
+      {id: 2, placeholder: 'karim', controler: 'tfest'},
+      {id: 3, placeholder: 'karim', controler: 'tefst'},
+      {id: 4, placeholder: 'karim', controler: 'tesft'}
+    ],
+    type: ActionTypes.RadioButton
+  },
+]
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent implements OnInit {
   title = 'code-chalenge';
+  acitonTypes = ActionTypes;
+  user = DefaulUser;
+
   dynamicForm: FormGroup = new FormGroup({});
   constructor(private fb: FormBuilder){}
 
   ngOnInit(){
-
+    this.initForm();
   }
 
+  // get user(){
+  //   return this.dynamicForm.get('user') as FormArray
+  // }
   
   initForm(){
-    this.dynamicForm = this.fb.group({
-      name: 'karim',
-      email: 'ali'
+    this.dynamicForm = this.fb.group({})
+    this.setDefaultUser()
+  }
+
+  private setDefaultUser(){
+    DefaulUser.forEach(field=>{
+      if(field.type !== this.acitonTypes.Checkbox){
+        this.dynamicForm.addControl(field.controler, this.fb.control(field.value))
+      }else{
+        this.dynamicForm.addControl(field.controler, this.fb.group(
+          this.setCheckBoxGroubValues(field.options)
+        ));
+      }
     })
+  }
+
+  private setCheckBoxGroubValues(source: Array<any>): {} {
+    const item = {}
+    
+    source.forEach(option=>{
+       item[option.controler] = option.value      
+    })
+    // console.log(this.dynamicForm.get(source.));
+    
+     return item
+
+    
+  }
+
+
+  convertJsonObjectToUserForm(){
+    // this.dynamicForm.addControl('name', this.fb.control(''))
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatRadioModule} from '@angular/material/radio';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSelectModule} from '@angular/material/select';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -13,8 +17,12 @@ import {MatSelectModule} from '@angular/material/select';
   ],
   imports: [
     BrowserModule,
+    MatRadioModule,
+    ReactiveFormsModule,
     MatFormFieldModule,
     MatSelectModule,
+    MatCheckboxModule,
+    MatInputModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
